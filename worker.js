804f3343228cc1ed5685c83c4c267a919ec3d547
shakamurikalyan <100946307+shakamurikalyan@users.noreply.github.com>
2022-05-09 09:23:50 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,35 +1,34 @@
-const { parentPort, workerData } = require('worker_threads');
-
+const {
+    parentPort,
+    workerData
+} = require('worker_threads');
 var ws;
-async function y(){
-    
-  ws=await require('./server').init()
-}
+async function y() {
 
-
-async function x(id){
-   await  y();
-   try{
-    if(ws){
-    await ws.login({
-        gatewayId: id
-    });
-    return("online");
+    ws = await require('./server').init()
 }
-    else{
-        return('not connected',ws);
-    }}
-    catch(e){
-        return('offline');
+async function x(id) {
+    await y();
+    try {
+        if (ws) {
+            let resp = await ws.login({
+                gatewayId: id
+            });
+            if (resp) {
+                return ("online");
+            } else {
+                return ('offline');
+            }
+        } else {
+            return ('not connected', ws);
+        }
+    } catch (e) {
+        return ('you are not authenticated');
     }
-    
+
 }
-parentPort.on('message',async  (param) => {
+parentPort.on('message', async (param) => {
+    console.log(param);
     const result = await x(param);
-  
-    // Access the workerData.
-  
-  
-    // return the result to main thread.
     parentPort.postMessage(result);
-  });
+});
